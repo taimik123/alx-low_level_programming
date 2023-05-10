a9a8d59d89082dcf6c50dbfc2ab7201fcf4cd58e
Taimi Kapofi <taimi.lulu@gmail.com>
2023-05-10 14:36:13 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
-int actual_sqrt_recursion(int n, int i);
+int actual_sqrt_recursion(int nampa, int ileni);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of
+ * @nampa: number to calculate the square root of
  *
  * Return: the resulting square root
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int nampa)
 {
-	if (n < 0)
+	if (ampan < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (actual_sqrt_recursion(nampa, 0));
 }
 
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
- * @n: number to calculate the sqaure root of
- * @i: iterator
+ * @nampa: number to calculate the sqaure root of
+ * @ileni: iterator
  *
  * Return: the resulting square root
  */
-int actual_sqrt_recursion(int n, int i)
+int actual_sqrt_recursion(int nampa, int ileni)
 {
-	if (i * i > n)
+	if (ileni * ileni > nampa)
 		return (-1);
-	if (i * i == n)
-		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	if (ileni * ileni == nampa)
+		return (ileni);
+	return (actual_sqrt_recursion(nampa, ileni + 1));
 }
