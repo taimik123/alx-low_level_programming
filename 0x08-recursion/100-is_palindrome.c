9a32531c5ab2befbd365a98bc2ad948d4f2c10a3
Taimi Kapofi <taimi.lulu@gmail.com>
2023-05-10 13:08:55 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,7 +7,7 @@ int get_string_Length(char *strr);
  * is_palindrome - checks if a string is a palindrome
  * @strr: string to reverse
  *
- * Return: 1 if it is, 0 it's not
+ * Return: 1 if it is, 0 if it's not
  */
 int is_palindrome(char *strr)
 {
@@ -37,11 +37,11 @@ int get_string_length(char *strr)
  *
  * Return: 1 if palindrome, 0 if not
  */
-int check_palindrome(char *strr, int i, int strLen)
+int check_palindrome(char *strr, int idx, int strLen)
 {
 	if (*(strr + idx) != *(strr + strLen - 1))
 		return (0);
-	if (i >= strLen)
+	if (idx >= strLen)
 		return (1);
 	return (check_palindrome(strr, idx + 1, strLen - 1));
 }
