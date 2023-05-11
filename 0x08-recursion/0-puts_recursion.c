66cdd37a0f5cf493ecb83bcc6b82a38e9b7bbbbe
Taimi Kapofi <taimi.lulu@gmail.com>
2023-05-11 04:18:59 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,48 +1,17 @@
-#include <stdio.h>
-
-/* Function prototype */
-void _puts_recursion(char *str);
-int _putchar(char c);
-
+#include "main.h"
 /**
- * main - Entry point
- *
+ * _puts_recursion - function like puts();
+ * @s: input
  * Return: Always 0 (Success)
  */
-int main(void)
+void _puts_recursion(char *s)
 {
-    char *str = "Hello, world!";
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 
-    _puts_recursion(str);
-
-    return 0;
-}
-
-/**
- * _puts_recursion - Function like puts()
- * @str: Input string
- */
-void _puts_recursion(char *str)
-{
-    if (*str)
-    {
-        _putchar(*str);
-        _puts_recursion(str + 1);
-    }
-    else
-    {
-        _putchar('\n');
-    }
-}
-
-/**
- * _putchar - Prints a single character
- * @c: The character to be printed
- *
- * Return: On success, returns the character to be printed.
- * On error, returns -1 and sets errno appropriately.
- */
-int _putchar(char c)
-{
-    return putchar(c);
+	else
+		_putchar('\n');
 }
